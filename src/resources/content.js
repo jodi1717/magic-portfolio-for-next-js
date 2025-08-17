4b164d114758716cd31dd8cdea6baec2e1e5fdd8
jodi1717 <odiex05@gmail.com>
2025-08-17 19:55:27 +0700

    Update content.js
    contact person

--- src/resources/content.js
+++ src/resources/content.js
@@ -1,8 +1,8 @@
 import { Logo } from "@once-ui-system/core";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Yodi",
+  lastName: "Jayadikarta",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
